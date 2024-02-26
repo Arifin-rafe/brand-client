e2bdb64bcea221c2f50caac541ae9d3b83e36141
Arifin-rafe <arafinrafe90@gmail.com>
2024-02-27 04:55:27 +0600

    delete added

--- src/components/MyCart/MyCart.jsx
+++ src/components/MyCart/MyCart.jsx
@@ -10,34 +10,34 @@ const MyCart = () => {
     console.log(brandName);
     const handleDelete = _id => {
         console.log(_id);
-        // Swal.fire({
-        //     title: 'Are you sure?',
-        //     text: "You won't be able to revert this!",
-        //     icon: 'warning',
-        //     showCancelButton: true,
-        //     confirmButtonColor: '#3085d6',
-        //     cancelButtonColor: '#d33',
-        //     confirmButtonText: 'Yes, delete it!'
-        // }).then((result) => {
-        //     if (result.isConfirmed) {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
 
-        //         fetch(`http://localhost:5000/cart/${_id}`,{
-        //             method:'DELETE'
+                fetch(`http://localhost:5000/cart/${_id}`,{
+                    method:'DELETE'
 
-        //         })
-        //             .then(res => res.json())
-        //             .then(data => {
-        //                 console.log(data);
-        //                 if (data.deletedCount > 0) {
-        //                       Swal.fire(
-        //                         'Deleted!',
-        //                         'Your file has been deleted.',
-        //                         'success'
-        //                       )
-        //                 }
-        //             })
-        //     }
-        // })
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                              Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                              )
+                        }
+                    })
+            }
+        })
     }
     return (
 
